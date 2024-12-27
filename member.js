85db0ae22282718d897c79cc9ea82734817d118e
Frank Migacz <111800314+frankmigacz@users.noreply.github.com>
2024-12-27 19:03:44 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  return {
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'NodeJS', 'MongoDB', 'Express'],
+    getSkills: function() {
+      return this.skills;
+    }
+  };
+}
